cb742f298186e44a8f09ebff4d79a45d9b6ef709
Eryx <evorui@gmail.com>
2018-10-11 23:40:06 +0800

    [CHG] link updated

---
+++ websrv/frontend/index.go
@@ -0,0 +1,51 @@
+// Copyright 2018 Eryx <evorui аt gmail dοt com>, All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package frontend
+
+import (
+	"github.com/hooto/httpsrv"
+
+	"github.com/hooto/htracker/config"
+)
+
+type Index struct {
+	*httpsrv.Controller
+}
+
+func (c Index) IndexAction() {
+
+	c.Response.Out.Header().Set("Cache-Control", "no-cache")
+
+	c.RenderString(`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+  <title>hooto Tracker | Open Source Application Performance Management</title>
+  <script src="/htracker/~/lessui/js/sea.js?v=` + config.VersionHash + `"></script>
+  <script src="/htracker/~/htracker/js/main.js?v=` + config.VersionHash + `"></script>
+  <link rel="stylesheet" href="/htracker/~/htracker/css/main.css?v=` + config.VersionHash + `" type="text/css">
+  <link rel="shortcut icon" type="image/x-icon" href="/htracker/~/htracker/img/system-monitor-dark.png">
+  <script type="text/javascript">
+    htracker.version = "` + config.VersionHash + `";
+	window.onload_hooks = [];
+    window.onload = htracker.Boot();
+  </script>
+</head>
+<body id="body-content">
+</body>
+</html>
+`)
+
+}
---
+++ webui/htracker/tpl/index.tpl
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/htracker/">
+    <img src="/htracker/~/htracker/img/system-monitor.svg" width="30" height="30">
+  </a>
+  <a class="navbar-brand" href="/htracker/">Perf Tracker</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul id="htracker-nav" class="navbar-nav" style="margin-left: 30px">
+      <li class="nav-item l4i-nav-item">
+        <a class="nav-link" href="#proj/index">Projects</a>
+      </li>
+      <li class="nav-item l4i-nav-item">
+        <a class="nav-link" href="#proc/index">Processes</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div id="htracker-module-layout">
+  <div id='htracker-module-navbar'>
+    <ul id='htracker-module-navbar-menus' class='htracker-module-nav'></ul>
+    <ul id='htracker-module-navbar-optools' class='htracker-module-nav htracker-nav-right'></ul>
+  </div>
+  <div id="htracker-module-content"></div>
+</div>
+
+<div class="htracker-body-footer">
+  <span>© 2018 <a href="https://github.com/hooto/htracker" target="_blank">hooto tracker {[=it.version]}</a>
+  &nbsp; is an open source application performance management</span>
+</div>
+
